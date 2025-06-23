e3d1876a54585fe74682116ee6ba35249e447ac0
rsj0131 <65953630+rsj0131@users.noreply.github.com>
2025-06-23 17:45:49 +0800

    Create scene.js

---
+++ pages/api/scene.js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+  const referer = req.headers.referer || '';
+  const userAgent = req.headers['user-agent'] || '';
+  const megaLink = process.env.SCENE_LINK;
+  const homeLink = process.env.HOME_LINK;
+
+  const isFromPatreonWeb = referer.includes("patreon.com");
+  const isFromKemWeb = referer.includes("kemono");
+  const isFromPatreonApp = userAgent.includes("iPhone") || userAgent.includes("Android");
+
+  console.log("Referer:", referer);
+  console.log("User-Agent:", userAgent);
+
+  if (isFromPatreonWeb || (isFromPatreonApp && !isFromKemWeb)) {
+    res.writeHead(302, { Location: megaLink });
+    res.end();
+  } else {
+    res.writeHead(302, { Location: homeLink });
+    res.end();
+  }
+}
